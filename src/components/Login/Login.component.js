86307b376a33a2450c95e8a883a740a83b235349
rahul-reptime <rahul@markettime.com>
2022-12-30 16:21:57 +0530

    update code

--- src/components/Login/Login.component.js
+++ src/components/Login/Login.component.js
@@ -1,44 +1,92 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { View, Text, StyleSheet, ImageBackground, TextInput, Button, Alert } from 'react-native'
-import commonStyle from '../../styles/common.style'
-import { AuthContext } from '../../context/AuthContext'
-import themeStyle from '../../styles/theme.style'
+import React, {useContext, useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TextInput,
+  Button,
+  Alert,
+} from 'react-native';
+import commonStyle from '../../styles/common.style';
+import {AuthContext} from '../../context/AuthContext';
+import themeStyle from '../../styles/theme.style';
 
 const Login = ({navigation}) => {
-    const {login} = useContext(AuthContext);
-    // console.warn(context)
-    const [name, setName] = useState(null);
-    const [email, setEmail] = useState(null);
-    const buttonPressEvent = () => {
-        if (!name || !name.trim() || !email || !email.trim()) {
-            Alert.alert("Plzz fill all the fields");
-        } else {
-            console.log(name, email);
-            login();
-        }
+  const {login} = useContext(AuthContext);
+  // console.warn(context)
+  let [name, setName] = useState({text: '', error: false});
+  let [email, setEmail] = useState({text: '', error: false});
+  const buttonPressEvent = () => {
+    if (!name.text.trim() || !email.text.trim()) {
+      Alert.alert('Please fill all the fields');
+    } else {
+      login(name, email);
     }
-    return (
-        <View style={commonStyle.container}>
-            <ImageBackground style={{flex:1}} resizeMode="cover" source={require('../../assets/images/login-splash-screen.jpg')}>
-                <View style={{flex:1, flexDirection:'column'}}>
-                    <View style={{backgroundColor: 'rgba(52, 52, 52, 0.8)', flex:2, padding: 20, justifyContent: 'center'}}>
-                        <Text style={commonStyle.h1Text}>Welcome</Text>
-                        <Text style={commonStyle.h2Text}>We are happy to see you here! Let's make our daily routine easier!</Text>
-                    </View>
-                    <View style={{backgroundColor:'white', flex:1, padding: 20}}>
-                        <TextInput value={name} onChangeText={setName} style={commonStyle.textInput} placeholder="What's your good name?" placeholderTextColor={'black'} autoComplete="name"></TextInput>
-                        <TextInput value={email} onChangeText={setEmail} style={commonStyle.textInput} placeholder="Enter your email address here" placeholderTextColor={'black'} autoComplete="email"></TextInput>
-                        <View style={commonStyle.buttonContainer}>
-                            <Button color={themeStyle.BUTTON_BG_COLOR} title="Let's Go" onPress={() => buttonPressEvent()}></Button>
-                        </View>
-                    </View>
-                </View>
-                {/* <Text style={commonStyle.headingText}>Login Component</Text> */}
-            </ImageBackground>
+  };
+  const validateName = (text) => {
+    name = {text: text, error: false};
+    setName(name);
+  };
+  const validateEmail = (text) => {
+    email = {text: text, error: false};
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+    if (reg.test(text) === false) {
+      // Email is Not Correct
+      email.error = true;
+    }
+    setEmail(email);
+  };
+  return (
+    <View style={commonStyle.container}>
+      <ImageBackground
+        style={{flex: 1}}
+        resizeMode="cover"
+        source={require('../../assets/images/login-splash-screen.jpg')}>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <View
+            style={{
+              backgroundColor: 'rgba(52, 52, 52, 0.8)',
+              flex: 2,
+              padding: 20,
+              justifyContent: 'center',
+            }}>
+            <Text style={commonStyle.h1Text}>Welcome</Text>
+            <Text style={commonStyle.h2Text}>
+              We are happy to see you here! Let's make our daily routine easier!
+            </Text>
+          </View>
+          <View style={{backgroundColor: 'white', flex: 1, padding: 20}}>
+            <TextInput
+              value={name.text}
+              onChangeText={text => validateName(text)}
+              style={commonStyle.textInput}
+              placeholder="What's your good name?"
+              placeholderTextColor={'black'}
+              autoComplete="name"></TextInput>
+            <TextInput
+              value={email.text}
+              onChangeText={text => validateEmail(text)}
+              style={[commonStyle.textInput,(email.error?styles.errorEmailInput:themeStyle.BUTTON_BG_COLOR)]}
+              placeholder="Enter your email address here"
+              placeholderTextColor={'black'}
+              autoComplete="email"></TextInput>
+            <View style={commonStyle.buttonContainer}>
+              <Button
+                color={themeStyle.BUTTON_BG_COLOR}
+                title="Let's Go"
+                onPress={() => buttonPressEvent()}></Button>
+            </View>
+          </View>
         </View>
-    )
-}
+        {/* <Text style={commonStyle.headingText}>Login Component</Text> */}
+      </ImageBackground>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-
+  errorEmailInput: {
+    borderColor: 'red',
+  },
 });
-export default Login
+export default Login;
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,19 @@
 import React, {createContext, useState, useEffect} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Base64 from '../utils/Base64';
+
 export const AuthContext = createContext();
 export const AuthProvider = ({children}) => {
   const [userToken, setUserToken] = useState(null);
   const [isLoading, setLoading] = useState(false);
-  const login = () => {
-    setLoading(true); 
-    setUserToken('ABC');
-    AsyncStorage.setItem('userToken', 'ABC');
+  const login = (name, email) => {
+    setLoading(true);
+    const encodedData = Base64.atob(name.text+':'+email.text);
+    console.log('Login Tap', name, email, encodedData);
+    // const decodedData = Base64.btoa(encodedData);
+    // console.log('Login>>>', name, email, decodedData);
+    // setUserToken('ABC');
+    // AsyncStorage.setItem('userToken', 'ABC');
     setLoading(false);
   };
   const logout = () => {
@@ -27,8 +33,9 @@ export const AuthProvider = ({children}) => {
     }
   };
   useEffect(() => {
+    console.log('useEffect AuthContext');
     // login();
-    // isLoggedIn();
+    isLoggedIn();
   }, []);
   return (
     <AuthContext.Provider value={{login, logout, userToken, isLoading}}>
